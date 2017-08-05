b7f0fa3d825598af1864434db0c5dbb61465ddf6
Grant Wu <grantwu2@gmail.com>
2017-08-05 11:35:26 -0700

    [SHELL]Add llvm/bin commands to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,8 @@ set -x GOPATH $HOME/Code/git/go
 # Add go binaries dir to PATH
 set -x PATH (go env GOPATH)/bin $PATH
 
+# Add llvm tools directory to PATH
+set -x PATH /usr/local/Cellar/llvm/4.0.1/bin $PATH
+
 # Add versioned user scripts directory to PATH
 set -x PATH ~/Code/git/dotfiles/grant_scripts $PATH
