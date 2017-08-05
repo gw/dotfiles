3dd68401fb655e3a9b2936acdcd5d095dd72be93
Grant Wu <grantwu2@gmail.com>
2017-08-05 11:35:06 -0700

    [SHELL]Add GOPATH to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,8 @@
+# Set GOPATH
+set -x GOPATH $HOME/Code/git/go
+
+# Add go binaries dir to PATH
+set -x PATH (go env GOPATH)/bin $PATH
+
 # Add versioned user scripts directory to PATH
 set -x PATH ~/Code/git/dotfiles/grant_scripts $PATH
