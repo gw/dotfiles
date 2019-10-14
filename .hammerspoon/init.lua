10399532dd8f4e76135f2da076af0ed980e86e1a
Grant Wu <gwu@stripe.com>
2019-10-14 15:19:11 -0700

    Add init.lua Hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,19 @@
+hs.hotkey.bind({"cmd", "alt"}, "w", 
+    function() hs.application.launchOrFocus("/Applications/Google Chrome.app") 
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "v", 
+    function() hs.application.launchOrFocus("/Applications/Visual Studio Code.app") 
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "s", 
+    function() hs.application.launchOrFocus("/Applications/Slack.app") 
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "t", 
+    function() hs.application.launchOrFocus("/Applications/iTerm.app") 
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "e", 
+    function() hs.application.launchOrFocus("/Applications/Emacs.app") 
+end)
