c9a741d235fa568dfc1faa174a02978da8f6d3d5
gw <4099071+gw@users.noreply.github.com>
2024-09-18 08:25:28 -0700

    [HAMMERSPOON] Save some more keybindings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,23 +1,44 @@
-hs.hotkey.bind({"cmd", "alt"}, "w",
+hs.hotkey.bind({"alt"}, "w",
     function() hs.application.launchOrFocus("/Applications/Google Chrome.app")
 end)
 
-hs.hotkey.bind({"cmd", "alt"}, "v",
-    function() hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+hs.hotkey.bind({"alt"}, "m",
+    function() hs.application.launchOrFocus("/System/Applications/Messages.app")
 end)
 
-hs.hotkey.bind({"cmd", "alt"}, "s",
+hs.hotkey.bind({"alt"}, "t",
+    function() hs.application.launchOrFocus("/Applications/kitty.app")
+end)
+
+hs.hotkey.bind({"alt"}, "c",
+    function() hs.application.launchOrFocus("/Applications/Obsidian.app")
+end)
+
+hs.hotkey.bind({"alt"}, "d",
+    function() hs.application.launchOrFocus("/Applications/Things3.app")
+end)
+
+-- This is set in Snippety's settings—just showing here as a reminder
+-- hs.hotkey.bind({"alt"}, "e",
+--     function() hs.application.launchOrFocus("/Applications/Snippety.app")
+-- end)
+
+hs.hotkey.bind({"alt"}, "g",
+    function() hs.application.launchOrFocus("/Applications/Github Desktop.app")
+end)
+
+hs.hotkey.bind({"alt"}, "v",
+    function() hs.application.launchOrFocus("/Applications/Cursor.app")
+end)
+
+hs.hotkey.bind({"alt"}, "s",
     function() hs.application.launchOrFocus("/Applications/Slack.app")
 end)
 
-hs.hotkey.bind({"cmd", "alt"}, "t",
-    function() hs.application.launchOrFocus("/Applications/iTerm.app")
+hs.hotkey.bind({"alt"}, "a",
+    function() hs.application.launchOrFocus("/Users/gwu/Applications/Chrome Apps.localized/Claude.app")
 end)
 
-hs.hotkey.bind({"cmd", "alt"}, "e",
-    function() hs.application.launchOrFocus("/Applications/Emacs.app")
-end)
-
-hs.hotkey.bind({"cmd", "alt"}, "g",
-    function() hs.application.launchOrFocus("/Applications/GitHub Desktop.app")
+hs.hotkey.bind({"alt"}, "z",
+    function() hs.application.launchOrFocus("/Applications/ChatGPT.app")
 end)
